96d536942d120ab40e9a28a6c6cbe70a1bd30ddb
Gorbunov Vladislav <wint-mil@mail.ru>
2023-06-09 14:32:57 +0300

    test commit 7

--- src/main/kotlin/com/Application.kt
+++ src/main/kotlin/com/Application.kt
@@ -9,7 +9,6 @@ import io.ktor.server.application.*
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
     configureSecurity()
     configureHTTP()
     configureSerialization()
